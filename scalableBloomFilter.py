5f25c17c3a3654c21afd0a2d2e12b220c742a1b5
emrebarkinn <emrebarkinn@gmail.com>
2019-01-30 18:10:25 +0300

    counting and scalable bloom filters implemented *counting bloom filter's union and intersection methods not implemented

--- scalableBloomFilter.py
+++ scalableBloomFilter.py
@@ -1,32 +1,53 @@
 import math
+
 from bloomFilter import BloomFilter
+from countingBloomFilter import CountingBloomFilter
+
 
 class ScalableBloomFilter(object):
     SMALL_GROWTH = 2
     LARGE_GROWTH = 4
 
-
-    def __init__(self, initial_items_count, fp_prob, growth=SMALL_GROWTH ,countable=False):
+    def __init__(self, initial_items_count=100, fp_prob=0.001, growth=SMALL_GROWTH, countable=False):
 
         self.initial_items_count = initial_items_count
         self.fp_prob = fp_prob
         self.growth = growth
 
         if countable:
-            pass #TODO countable bloom filter constructor will added
+            self.create_filter = CountingBloomFilter
         else:
-            self.create_filter = lambda: BloomFilter(initial_items_count, fp_prob)
+            self.create_filter = BloomFilter
 
+        self.bloom_filters = []
 
+    def add(self, item):
 
+        if item in self:
+            return True
+        if not self.bloom_filters:
+            temp_filter = self.create_filter(self.initial_items_count, self.fp_prob)
+            self.bloom_filters.append(temp_filter)
+        else:
+            temp_filter = self.bloom_filters[-1]
+            if temp_filter.count >= temp_filter.item_low_count:
+                temp_filter = self.create_filter(temp_filter.size * self.growth, self.fp_prob)  # TODO check fp prob
+                self.bloom_filters.append(temp_filter)
 
+        temp_filter.add(item)
 
+        return False
 
+    def get_total_size(self):
 
+        return sum([temp.size for temp in self.bloom_filters])
 
+    def __contains__(self, item):
 
-
-
+        for temp in reversed(self.bloom_filters):
+            if item in temp:
+                return True
+        return False
 
     @classmethod
     def get_size(self, n, p):
@@ -42,7 +63,6 @@ class ScalableBloomFilter(object):
         m = -(n * math.log(p)) / (math.log(2) ** 2)
         return int(m)
 
-
     @classmethod
     def get_hash_count(self, m, n):
         '''
@@ -57,4 +77,3 @@ class ScalableBloomFilter(object):
         '''
         k = (m / n) * math.log(2)
         return int(k)
-
